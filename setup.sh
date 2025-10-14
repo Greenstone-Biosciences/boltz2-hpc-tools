baff475baeb19153e51baa9dd8544db0f669394f
jleitz <leitzj@gmail.com>
2025-10-13 17:07:37 -0700

    Further debugging of setup.sh

--- setup.sh
+++ setup.sh
@@ -8,13 +8,14 @@ NUTZ_PATH=${NUTZ_PATH:-"$HOME/Software/boltz-hpc-tools"}
 read -p "Conda environment name [boltz]: " BOLTZ_CONDA_ENV
 BOLTZ_CONDA_ENV=${BOLTZ_CONDA_ENV:-"boltz"}
 
-read -p "Path to the affinity_template yaml [$PWD]: "AFFINITY_TEMPLATE
+read -p "Path to the affinity_template yaml [$PWD]: " AFFINITY_TEMPLATE
 AFFINITY_TEMPLATE=${AFFINITY_TEMPLATE:-"$HOME/Software/boltz-hpc-tools"}
+
 # Replace in all scripts
 for script in prep_batch_boltz Nutz.sh analyze_batch_boltz; do
-    sed -i "s|NUTZ_PATH=.*|NUTZ_PATH=\"$NUTZ_PATH\"|" $script
-    sed -i "s|BOLTZ_CONDA_ENV=.*|BOLTZ_CONDA_ENV=\"$BOLTZ_CONDA_ENV\"|" $script
-    sed -i "s|AFFINITY_TEMPLATE=.*|AFFINITY_TEMPLATE=\"$AFFINITY_TEMPLATE\"|" $script
+    sed -i "s|NUTZ_PATH|$NUTZ_PATH|g" $script
+    sed -i "s|BOLTZ_CONDA_ENV|$BOLTZ_CONDA_ENV|g" $script
+    sed -i "s|AFFINITY_TEMPLATE|$AFFINITY_TEMPLATE|g" $script
 done
 
 echo "Setup complete!"
